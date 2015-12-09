ebf5d30ab2a379185fbdbeee38c430d1ff8e36e9
Lukas <h4xxflame@gmail.com>
2015-12-09 12:49:20 +0100

    asd

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ class Display(Tk):
     def __init__(self):
         Tk.__init__(self)
         container = Tk.Frame(self)
-        container.pack(side="top", fill="both", expand = True)
+        container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-        self.frames =  {}
-        frame = StartPage(container,self)
+        self.frames = {}
+        frame = StartPage(container,vself)
         self.frames[StartPage] = frame
         frame.grid(row=0, column=0, sticky="nsew")
 
@@ -25,6 +25,7 @@ class StartPage(Tk.frame):
     def __init__(self, parent, controller):
         Tk.Frame.__init__(self, parent)
         label = Tk.Label(self, text="asdasddas")
+        #asdasd
         label.pack()
 
 
